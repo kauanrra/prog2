534699a86e19115dc3e36aa729b6163b785fd1f5
kauanrra <kauan.rra@gmail.com>
2026-04-05 20:31:52 -0300

    Create ex-3.cpp

---
+++ aula-2/ex-3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float valor1, valor2, valor3;
+
+    cout << "Digite o primeiro valor: ";
+    cin >> valor1;
+
+    cout << "Digite o segundo valor: ";
+    cin >> valor2;
+
+    cout << "Digite o terceiro valor: ";
+    cin >> valor3;
+
+    cout << "\nValores digitados: " << valor1 << ", " << valor2 << ", " << valor3 << "\n";
+
+    return 0;
+}
