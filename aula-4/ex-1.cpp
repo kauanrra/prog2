16c06e664d7d2d0b33cf37a6124f8f34fabe1b11
kauanrra <kauan.rra@gmail.com>
2026-04-16 14:55:16 -0300

    Update ex-1.cpp

--- aula-4/ex-1.cpp
+++ aula-4/ex-1.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+using namespace std;
+
+struct data {
+    int dia;
+    int mes;
+    int ano;
+};
+
+int main() {
+    data dataAtual;
+
+    cout << "Digite o dia atual: ";
+    cin >> dataAtual.dia;
+    
+    cout << "Digite o mes atual: ";
+    cin >> dataAtual.mes;
+    
+    cout << "Digite o ano atual: ";
+    cin >> dataAtual.ano;
+
+    cout << "\nA data informada e: " << dataAtual.dia << " / " 
+         << dataAtual.mes << " / " << dataAtual.ano << endl;
+
+    return 0;
+}
